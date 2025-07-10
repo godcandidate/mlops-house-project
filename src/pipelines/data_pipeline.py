9f5a03612c66c9d0bee65da50effcc5e42fa521c
godcandidate <godcandidate101@gmail.com>
2025-07-10 09:22:15 +0000

    fix(run): update data_processing_pipeline to include code_repository parameter

--- src/pipelines/data_pipeline.py
+++ src/pipelines/data_pipeline.py
@@ -7,7 +7,7 @@ from pathlib import Path
 PROJECT_ROOT = Path(__file__).parent.parent.parent
 
 
-@pipeline(enable_cache=False)
+@pipeline(enable_cache=False, code_repository="mlops-house-project")
 def data_processing_pipeline(
 ):
     """Modular data processing pipeline."""
--- src/run_data_pipeline.py
+++ src/run_data_pipeline.py
@@ -1,9 +1,5 @@
 from pipelines.data_pipeline import data_processing_pipeline
-from zenml.client import Client
 
 if __name__ == "__main__":
-    # Get the registered GitHub code repository
-    repo = Client().get_code_repository("mlops-house-project")
 
-    # Run the pipeline with the associated code repository
-    data_processing_pipeline.run(code_repository=repo)
+    data_processing_pipeline()
