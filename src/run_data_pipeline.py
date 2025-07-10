a508a633e383af8320f31856640b92fd61f303ea
godcandidate <godcandidate101@gmail.com>
2025-07-10 08:57:04 +0000

    feat(run): enhance run_data_pipeline to link with GitHub repository

--- src/run_data_pipeline.py
+++ src/run_data_pipeline.py
@@ -1,4 +1,9 @@
 from pipelines.data_pipeline import data_processing_pipeline
+from zenml.code_repositories import get_code_repository
 
 if __name__ == "__main__":
-    data_processing_pipeline()
+    # Link to the registered GitHub repository
+    repo = get_code_repository("mlops-house-project")
+
+    # Run the pipeline with the code repository attached
+    data_processing_pipeline().run(code_repository=repo)
