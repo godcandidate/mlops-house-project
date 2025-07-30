3796b7a8f0b3c3b8e7b99eb50809cf85ae3b0919
godcandidate <godcandidate101@gmail.com>
2025-07-30 14:16:16 +0000

    chore: remove unused API files and Docker configuration

--- src/api/utils.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
-
-class ModelConfig:
-    MODE = os.getenv("MODEL_MODE", "default")  # "default" or "ab"
